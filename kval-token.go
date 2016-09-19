7ae17db2caebc8d30b3ba5763cbec90da337dbb5
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-09-19 18:00:33 +1200

    [feature] lower case function keywords.

--- kval-token.go
+++ kval-token.go
@@ -38,8 +38,17 @@ const (
 //may be unecessary redundancy...
 var KeywordMap = map[string]int{
    "INS":   0x1,         // Insert
+   "ins":   0x1,
+
    "GET":   0x2,         // Get values
+   "get":   0x2, 
+
    "LIS":   0x3,         // Check existence
+   "lis":   0x3, 
+
    "DEL":   0x4,         // Delete values
+   "del":   0x4,
+
    "REN":   0x5,         // Rename values   
+   "ren":   0x5,
 }
