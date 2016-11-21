045b2cbafe59fb600abfccfc52362454bb982152
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 15:31:22 +1300

    [documentation] Trying to get doc.go to work.

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
-// KVAL (Key Value Access Language) Parser implemented in Go that can be used by 
-// anyone who wishes to make a new binding for KVAL (Key Value Access Language) 
+// Package kvalparse implements a parser for KVAL (Key Value Access Language) 
+// that can be used by anyone who wishes to make a new binding for KVAL  
 // in the same language. Current bindings exist for BoltDb and can be found 
 // here: https://github.com/kval-access-language/kval-boltdb
 
