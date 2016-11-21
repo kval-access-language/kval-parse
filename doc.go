41a92909b6b0934250fe2222e27e63d898ea061b
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 15:37:00 +1300

    [documentation] Attempting to get GoDoc to work.

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
-// Package kvalparse implements a parser for KVAL (Key Value Access Language) 
-// that can be used by anyone who wishes to make a new binding for KVAL  
-// in the same language. Current bindings exist for BoltDb and can be found 
-// here: https://github.com/kval-access-language/kval-boltdb
-
+/* 
+	Package kvalparse implements a parser for KVAL (Key Value Access Language) 
+	that can be used by anyone who wishes to make a new binding for KVAL  
+	in the same language. Current bindings exist for BoltDb and can be found 
+	here: https://github.com/kval-access-language/kval-boltdb
+*/
 package kvalparse
