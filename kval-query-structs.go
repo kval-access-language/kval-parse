7371838d9ccd48ea4ea42fcad1bffd04d1f38f84
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 14:58:00 +1300

    [refactor] Working with Gofmt.

--- kval-query-structs.go
+++ kval-query-structs.go
@@ -2,11 +2,13 @@ package kvalparse
 
 import "github.com/kval-access-language/kval-scanner"
 
+// KQuery struct used to breakdown a query string in a way that can
+// be utilised by the calling code more easily.
 type KQuery struct {
-	Function kvalscanner.Token
-	Buckets  []string
-	Key      string
-	Value    string
-	Newname  string
-	Regex    bool
+	Function kvalscanner.Token // Function being called from KVAL
+	Buckets  []string          // The Bucket or Buckets the query wants to create/interrogate
+	Key      string            // The Key the query wants to create/interrogate
+	Value    string            // The Value the query wants to create/interrogate
+	Newname  string            // If renaming a Bucket or Key, the new name is found here
+	Regex    bool              // If regex is involved in the query anywhere, True here
 }
