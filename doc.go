220f5043b9aefd21636ef929eb5d58b5cb519439
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 15:27:25 +1300

    [documentation] Further information on the package.

---
+++ doc.go
@@ -0,0 +1,6 @@
+package kvalparse
+
+// KVAL (Key Value Access Language) Parser implemented in Go that can be used by 
+// anyone who wishes to make a new binding for KVAL (Key Value Access Language) 
+// in the same language. Current bindings exist for BoltDb and can be found 
+// here: https://github.com/kval-access-language/kval-boltdb
