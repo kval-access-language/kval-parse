b8fb81cd8f632ab1eecf8a55b10c0d70cfcc9e37
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-11-21 15:50:59 +1300

    [documentation] Improving the documentation.

--- doc.go
+++ doc.go
@@ -3,5 +3,8 @@
 	that can be used by anyone who wishes to make a new binding for KVAL  
 	in the same language. Current bindings exist for BoltDb and can be found 
 	here: https://github.com/kval-access-language/kval-boltdb
+
+	The most up-to-date specification for KVAL can be found here:
+	https://github.com/kval-access-language/kval
 */
 package kvalparse
